480dacf9efe12924a4996e96fa55933b7336e1d0
Thomas HOULLIER <pro@houllier.net>
2024-12-07 15:28:56 +0100

    Day07 part2 done.

--- day_07/day_07.lua
+++ day_07/day_07.lua
@@ -149,3 +149,112 @@ example_part1_result = accumulate_solved_problems(example_problems)
 part1_result = accumulate_solved_problems(input_problems)
 print("Part 1 example result: ", example_part1_result)
 print("Part 1 result: ", part1_result)
+
+-- # Part 2
+-- Given the size of the sequences, we can probably still do it
+-- in the "dumb" way.
+
+-- Concatenation operator
+function concat_operator (num1, num2)
+  return tonumber(tostring(num1) .. tostring(num2))
+end
+
+-- We now represent a sequence of operations as {op1, op2, ...}
+-- with an op either 0 (addition), 1 (multiplication) or 2 (concat)
+
+-- Next possible operation sequence
+-- We iterate like {0, 0, 0}, {1, 0, 0}, {2, 0, 0}, {0, 1, 0} ...
+-- until {2, 2, 2}
+-- Operates in-place, does not check for end.
+function next_possible_operations (operations)
+  operations[1] = math.fmod(operations[1] + 1, 3)
+  if operations[1] == 0 then -- Carry
+    local n = #operations
+    local i = 2
+    while i <= n do
+      operations[i] = math.fmod(operations[i] + 1, 3)
+      if operations[i] ~= 0 then return
+      end
+      i = i + 1
+    end
+  end
+end
+
+-- Iterator over all possible operations for sequence of numbers of length n.
+function possible_operations (n)
+  local operations = {}
+  local i_ops = 0
+  local n_ops = math.floor(3 ^ (n-1) + 0.5)
+  for i = 1, n - 1 do
+    table.insert(operations, 2)
+  end
+  return function ()
+    i_ops = i_ops + 1
+    if i_ops <= n_ops then
+      next_possible_operations(operations)
+      return operations
+    end
+  end
+end
+
+print("Possible operations for sequence of 4: ")
+for ops in possible_operations(4) do
+  print(table.concat(ops, ' '))
+end
+
+-- Compute a result given numbers and the new representation for the operations.
+function compute_test_with_concat (numbers, operations)
+  local result = numbers[1]
+  for inum = 2, #numbers do
+    local num = numbers[inum]
+    local op = operations[inum - 1]
+    if op == 0 then -- Addition
+      result = result + num
+    elseif op == 1 then -- Multiplication
+      result = result * num
+    else -- op == 2 -- Concatenation
+      result = concat_operator(result, num)
+    end
+  end
+  return result
+end
+
+print("Example possible result for problem 2: (+, ||)")
+ex_prob2_result = compute_test_with_concat (example_problems[2][2],
+                                            {0, 2})
+
+-- Whether a given problem has a solution with concatenation included
+-- in the possible operations
+function problem_has_solution_with_concat (problem)
+  local test_value = problem[1]
+  local numbers = problem[2]
+  for operations in possible_operations(#numbers) do
+    local result_value = compute_test_with_concat(numbers, operations)
+    if result_value == test_value then
+      return true
+    end
+  end
+  return false -- We tried everything and couldn't find the solution.
+end
+
+print("Part 2 example problems: ")
+for i, problem in ipairs(example_problems) do
+  print("Problem ", i, " has solution? ",
+        problem_has_solution_with_concat(problem))
+end
+
+function accumulate_solved_problems_with_concat (problems)
+  local result = 0
+  for _, problem in ipairs(problems) do
+    if problem_has_solution_with_concat(problem) then
+      result = result + problem[1]
+    end
+  end
+  return result
+end
+
+example_part2_result = accumulate_solved_problems_with_concat(example_problems)
+print("Part 2 example result: ", example_part2_result)
+-- Takes 26sec to run
+part2_result = accumulate_solved_problems_with_concat(input_problems)
+print(part2_result)
