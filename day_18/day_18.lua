15ec61b10843486c22cea57454276ae6cb7ad374
Thomas HOULLIER <pro@houllier.net>
2024-12-19 20:08:21 +0100

    Day18 part 1 done.

---
+++ day_18/day_18.lua
@@ -0,0 +1,159 @@
+-- Day 18
+package.path = '../utils/?.lua;' .. package.path
+local utils = require "utils"
+
+input_str = utils.file_to_string("input.txt")
+ex_str = "5,4\n"
+      .. "4,2\n"
+      .. "4,5\n"
+      .. "3,0\n"
+      .. "2,1\n"
+      .. "6,3\n"
+      .. "2,4\n"
+      .. "1,5\n"
+      .. "0,6\n"
+      .. "3,3\n"
+      .. "2,6\n"
+      .. "5,1\n"
+      .. "1,2\n"
+      .. "5,5\n"
+      .. "2,5\n"
+      .. "6,5\n"
+      .. "1,4\n"
+      .. "0,4\n"
+      .. "6,4\n"
+      .. "1,1\n"
+      .. "6,1\n"
+      .. "1,0\n"
+      .. "0,5\n"
+      .. "1,6\n"
+      .. "2,0\n"
+input_dims = {71, 71}
+ex_dims = {7, 7}
+input_map = utils.full(input_dims, '.')
+ex_map = utils.full(ex_dims, '.')
+
+ex_start = {1, 1}
+ex_stop = {7, 7}
+ex_map[ex_stop[1]][ex_stop[2]] = 'E'
+
+input_start = {1, 1}
+input_stop = {71, 71}
+input_map[input_stop[1]][input_stop[2]] = 'E'
+
+-- # Parsing
+
+-- Parse the byte positions (use 1-index)
+function parse_bytes (str)
+  local positions = {}
+  for line in utils.lines(str) do
+    local row = {}
+    for int in utils.integers(line) do
+      local index = tonumber(int) + 1
+      table.insert(row, index)
+    end
+    -- Put the Y index as i, X as j
+    local temp = row[1]
+    row[1] = row[2]
+    row[2] = temp
+    table.insert(positions, row)
+  end
+  return positions
+end
+
+ex_positions = parse_bytes(ex_str)
+print("Example positions: ")
+for _, pos in ipairs(ex_positions) do
+  print(pos[1], pos[2])
+end
+input_positions = parse_bytes(input_str)
+
+-- # Part 1
+-- No worries here.
+
+-- Return the map obtained after dropping n bytes on it.
+function drop_bytes (_map, byte_positions, n_bytes)
+  local map = utils.copy_matrix(_map)
+  for ibyte = 1, n_bytes do
+    local pos = byte_positions[ibyte]
+    map[pos[1]][pos[2]] = '#'
+  end
+  return map
+end
+
+ex_cmap = drop_bytes(ex_map, ex_positions, 12)
+print("Corrupted example map: ")
+utils.print_matrix(ex_cmap)
+input_cmap = drop_bytes(input_map, input_positions, 1024)
+print("Corrupted input map: ")
+utils.print_matrix(input_cmap)
+
+-- We do something similar to what we did for the reindeer.
+-- We prune the paths by storing the minimal cost in a matrix.
+function positions_in_cross (position, dims)
+  local i = position[1]
+  local j = position[2]
+  local nrows = dims[1]
+  local ncols = dims[2]
+  local tried_position = -1
+  local rand_position = math.random(4) - 1
+  return function ()
+    while tried_position < 4 do
+      tried_position = tried_position + 1
+      if (tried_position + rand_position) % 4 == 0 and i-1 >= 1 then
+        return {i-1, j}, tried_position -- UP
+      elseif (tried_position + rand_position) % 4 == 1 and j+1 <= ncols then
+        return {i, j+1}, tried_position -- RIGHT
+      elseif (tried_position + rand_position) % 4 == 2 and i+1 <= nrows then
+        return {i+1, j}, tried_position -- DOWN
+      elseif j-1 >= 1 then
+        return {i, j-1}, tried_position -- LEFT
+      end
+    end
+  end
+end
+
+function search_iter (map, dims, cur_pos, cur_cost,
+                      cost_matrix, minimal_cost, visited_matrix)
+  if cur_cost >= minimal_cost[1] then
+    return false
+  end
+  local cell = utils.matrix_el(map, cur_pos)
+  if cell == '#' then
+    return false
+  end
+  local cell_cost = utils.matrix_el(cost_matrix, cur_pos)
+  if cur_cost >= cell_cost then
+    return false
+  end
+  cost_matrix[cur_pos[1]][cur_pos[2]] = math.min(cell_cost, cur_cost)
+  if cell == 'E' then
+    print("HAHA")
+    minimal_cost[1] = math.min(minimal_cost[1], cur_cost)
+    return true
+  end
+  visited_matrix[cur_pos[1]][cur_pos[2]] = 'O'
+  print(cur_cost, "Current minimum: ", minimal_cost[1])
+  utils.print_matrix(visited_matrix)
+  -- Else we continue searching
+  for next_pos in positions_in_cross(cur_pos, dims) do
+    search_iter(map, dims, next_pos, cur_cost + 1, cost_matrix, minimal_cost,
+                visited_matrix)
+  end
+end
+
+function search_minimal_cost (map, start_pos)
+  local dims = utils.matrix_shape(map)
+  local mincost_a_priori = 500
+  local cost_matrix = utils.full(dims, mincost_a_priori)
+  local visited_matrix = utils.copy_matrix(map)
+  search_iter(map, dims, start_pos, 0, cost_matrix, {mincost_a_priori},
+              visited_matrix)
+  -- The minimal cost is then just what is stored in the end cell.
+  return utils.matrix_el(cost_matrix, dims)
+end
+
+ex_mincost = search_minimal_cost(ex_cmap, ex_start)
+print("Example minimal cost: ", ex_mincost)
+input_mincost = search_minimal_cost(input_cmap, input_start)
+print("Part 1 result: ", input_mincost) -- Takes some minutes to run.
